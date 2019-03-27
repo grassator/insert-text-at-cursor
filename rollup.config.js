90e74dc3091541f16a2c936b046ea0f46715dc38
Christophe Coevoet <stof@notk.org>
2019-03-27 09:59:08 +0100

    Add a new dist file in ES format but processed with babel

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from "rollup-plugin-babel";
 
 const isProduction = process.env.NODE_ENV === "production";
 
-export default {
+export default [{
   input: "index.js",
   output: {
     file: `dist/index.umd${isProduction ? ".min" : ""}.js`,
@@ -12,4 +12,13 @@ export default {
     sourcemap: true
   },
   plugins: [babel(), isProduction && uglify()]
-};
+}, {
+  input: "index.js",
+  output: {
+    file: "dist/index.esm.js",
+    format: "es",
+    name: "insertTextAtCursor",
+    sourcemap: true
+  },
+  plugins: [babel()]
+}];
