474950bb7e41db121e72d35a800c3dd7f4df3feb
Dmitriy Kubyshkin <dmitriy@kubyshkin.name>
2019-04-10 20:06:11 +0200

    Updated dependencies and bump the version

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,27 @@
-import uglify from "rollup-plugin-uglify";
+import { uglify } from "rollup-plugin-uglify";
 import babel from "rollup-plugin-babel";
 
 const isProduction = process.env.NODE_ENV === "production";
 
-export default [{
-  input: "index.js",
-  output: {
-    file: `dist/index.umd${isProduction ? ".min" : ""}.js`,
-    format: "umd",
-    name: "insertTextAtCursor",
-    sourcemap: true
+export default [
+  {
+    input: "index.js",
+    output: {
+      file: `dist/index.umd${isProduction ? ".min" : ""}.js`,
+      format: "umd",
+      name: "insertTextAtCursor",
+      sourcemap: true
+    },
+    plugins: [babel(), isProduction && uglify()]
   },
-  plugins: [babel(), isProduction && uglify()]
-}, {
-  input: "index.js",
-  output: {
-    file: "dist/index.esm.js",
-    format: "es",
-    name: "insertTextAtCursor",
-    sourcemap: true
-  },
-  plugins: [babel()]
-}];
+  {
+    input: "index.js",
+    output: {
+      file: "dist/index.esm.js",
+      format: "es",
+      name: "insertTextAtCursor",
+      sourcemap: true
+    },
+    plugins: [babel()]
+  }
+];
